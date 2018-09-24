c74fd8baad71dd50c9c548aeea905b13ffe37e25
Joseph Parker <jparker010190@gmail.com>
2018-09-24 00:17:58 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,37 @@
 class ProductsController < ApplicationController
+  def index
+    @product = Product.all
+  end
+  def new
+    @product = Product.new
+  end
+  def create
+    product = Product.create(product_params)
+    redirect_to products_path
+  end
+  def inventory
+    id = get_product_id
+    product = Product.find(id)
 
+    if product.inventory == 0
+      render plain: "false"
+    else
+      render plain: "true"
+    end
+  end
+  def description
+    id = get_product_id
+    product = Product.find(id)
+
+    render plain: product.description
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
+  def get_product_id
+    params[:id] ? params[:id] : params[:product_id]
+  end
 end
